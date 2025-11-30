c5cf79065543e7b069bd69921607e602e7d065bc
execaus <execaus@gmail.com>
2025-11-30 13:57:51 +0500

    Добавляет миграцию с созданием схемы для приложения

--- migrations/20251130084722_create_currencies.sql
+++ migrations/20251130084722_create_currencies.sql
@@ -1,20 +1,20 @@
 -- +goose Up
 -- +goose StatementBegin
 
-CREATE TABLE IF NOT EXISTS currencies (
+CREATE TABLE IF NOT EXISTS app.currencies (
     code VARCHAR(10) PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
 );
 
-COMMENT ON TABLE currencies IS 'Справочник валют';
-COMMENT ON COLUMN currencies.code IS 'Уникальный код валюты (ISO)';
-COMMENT ON COLUMN currencies.name IS 'Название валюты';
+COMMENT ON TABLE app.currencies IS 'Справочник валют';
+COMMENT ON COLUMN app.currencies.code IS 'Уникальный код валюты (ISO)';
+COMMENT ON COLUMN app.currencies.name IS 'Название валюты';
 
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 
-DROP TABLE IF EXISTS currencies;
+DROP TABLE IF EXISTS app.currencies;
 
 -- +goose StatementEnd
